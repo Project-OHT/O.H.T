73c5123b30631fb831560b962881231321abb83a
Project-OHT <85153055+Project-OHT@users.noreply.github.com>
2021-06-14 08:55:12 +0700

    Update oht.py

--- oht.py
+++ oht.py
@@ -68,6 +68,51 @@ def encrypt(a):
         a = a.replace(ifcs[key],keys[key])
     return a
 
+def UARand():
+    base = 'Mozilla /5.0 ('
+    plt = ['Linux; ','Windows NT ','Macintosh; Intel Mac OS X ','X11; '][random.randint(0,3)]
+    base += plt
+    if plt=='Linux; ':
+        osp = ['U; ','Android '][random.randint(0,1)]
+        if osp=='Android ':
+            osp += ['4.','5.'][random.randint(0,1)]+['0.','2.','4.'][random.randint(0,2)]+str(random.randint(1,4))
+        elif osp=='U; ':
+            osp += 'Android '+['4.','5.'][random.randint(0,1)]+['0.','2.','4.'][random.randint(0,2)]+str(random.randint(1,4))
+        base+=osp+')'
+    elif plt=='Windows NT ':
+        osp = str(random.randint(5,10))+'.'
+        if not osp=='10':
+            str(random.randint(1,3))
+        
+        if random.randint(1,2)==2:
+            osp += '; WOW64'
+            if random.randint(1,2)==2:
+                osp += '; Trident/7.0; rv:11.0'
+        else:
+            osp += '; Win64; x64'
+        base += osp+')'
+    elif plt=='Macintosh; Intel Mac OS X ':
+        base += '10_'+str(random.randint(6,10))+'_'+str(random.randint(2,5))+')'
+    elif plt=='X11; ':
+        osp = ['CrOS x86_64 ','Ubuntu; ','U; ','Linux i686','Linux x86_64'][random.randint(0,4)]+')'
+        if osp=='CrOS x86_64 ':
+            osp += str(random.randint(6500,7077))+'.'+str(random.randint(52,134))+'.0)'
+            base += osp
+        elif osp=='Ubuntu; ':
+            osp += ['Linux i686','Linux x86_64'][random.randint(0,1)]
+            if random.randint(1,2)==2:
+                osp += '; rv:'+str(random.randint(33,40))+'.0'
+        elif osp=='U; ':
+            osp += ['Linux i686','Linux x86_64'][random.randint(0,1)]+', en-US'
+        base += osp
+    ie = [' Gecko/20100101',' AppleWebKit/537.36 (KHTML, like Gecko)'][random.randint(0,1)]
+    if ie==' AppleWebKit/537.36 (KHTML, like Gecko)':
+        ie += ' Chrome/'+str(random.randint(31,91))+'.0.'+str(random.randint(1650,4472))+'.'+str(random.randint(0,95))+' Safari/537.36'
+    elif ie==' Gecko/20100101':
+        ie += ' Firefox/'+str(random.randint(29,40))
+    base += ie
+    return base
+
 def randToken():
     a = random.randint(75,80)
     b = []
@@ -78,6 +123,37 @@ def randToken():
     for i in range(0,len(b)):
         e = e+b[i]
     return e
+
+def afind(an):
+    fap = ['ig','git','twch','twt','arcv','blg','strm','chs','yho','gml','wml']
+    if requests.get('https://www.instagram.com/'+an,headers={'User-Agent': UARand()}).status_code == 200: ig = 'https://www.instagram.com/'+an
+    else: ig = None
+    if requests.get('https://github.com/'+an,headers={'User-Agent': UARand()}).status_code == 200: git = 'https://github.com/'+an
+    else: git = None
+    if requests.get('https://www.twitch.tv/'+an,headers={'User-Agent': UARand()}).status_code == 200: twch = 'https://www.twitch.tv/'+an
+    else: twch = None
+    if requests.get('https://twitter.com/'+an,headers={'User-Agent': UARand()}).status_code == 200: twt = 'https://twitter.com/'+an
+    else: twt = None
+    if requests.get('https://archive.org/details/@'+an.lower(),headers={'User-Agent': UARand()}).status_code == 200: arcv = 'https://twitter.com/'+an
+    else: arcv = None
+    if requests.get('https://'+an.lower()+'.blogspot.com',headers={'User-Agent': UARand()}).status_code == 200: blg = 'https://'+an.lower()+'.blogspot.com'
+    else: blg = None
+    if requests.get('https://streamlabs.com/'+an.lower(),headers={'User-Agent': UARand()}).status_code == 200: strm = 'https://streamlabs.com/'+an.lower()
+    else: strm = None
+    if requests.get('https://www.chess.com/member/'+an.lower(),headers={'User-Agent': UARand()}).status_code == 200: chs = 'https://www.chess.com/member/'+an.lower()
+    else: chs = None
+    if requests.get('https://isitarealemail.com/api/email/validate',params={'email': an+'@yahoo.com'}).json()['status'] == 'valid': yho = an+'@yahoo.com'
+    else: yho = None
+    if requests.get('https://isitarealemail.com/api/email/validate',params={'email': an+'@gmail.com'}).json()['status'] == 'valid': gml = an+'@gmail.com'
+    else: gml = None
+    if requests.get('https://isitarealemail.com/api/email/validate',params={'email': an+'@mail.com'}).json()['status'] == 'valid': wml = an+'@mail.com'
+    else: wml = None
+    if not cpt:
+        print('['+Fore.GREEN+'INF'+Fore.RESET+'] Found :')
+    else:
+        print('[INF] Found :')
+    for p in fap:
+        exec('if '+p+' != None: print(\'    \'+'+p+')')
 
 def restore():
     print('Restoring...')
@@ -97,7 +173,7 @@ if arg.mimt:
     try:
         while True:
             mitm()
-            time.sleep(1.5)
+            time.sleep(2.5)
     except KeyboardInterrupt:
         restore()
 
@@ -130,4 +206,3 @@ Option :
 elif arg.fsa:
     acn = input('Account Name : ')
     afind(acn)
-    
